7ff042b385de8b2c621a179d11f6d76fc6464c35
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-18 12:53:54 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -49,105 +49,58 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: no1Controller,
-                    style: const TextStyle(fontSize: 20, color: Colors.black),
-                    decoration: InputDecoration(
-                      labelText: 'Enter number 1',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    keyboardType: TextInputType.number,
-                    controller: no2Controller,
-                    style: const TextStyle(fontSize: 20, color: Colors.black),
-                    decoration: InputDecoration(
-                      labelText: 'Enter number 2',
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double no2) => no1 + no2, 'Add');
-                        },
-                        text: 'Add',
-                      ),
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double no2) => no1 - no2, 'Subtract');
-                        },
-                        text: 'Subtract',
-                      ),
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double no2) => no1 * no2, 'Multiply');
-                        },
-                        text: 'Multiply',
-                      ),
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult(
-                                (double no1, double no2) => no2 != 0 ? no1 / no2 : double.infinity,
-                            'Divide',
-                          );
-                        },
-                        text: 'Divide',
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double _) => pow(no1, 2).toDouble(), 'Square');
-                        },
-                        text: 'Square',
-                      ),
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double _) => pow(no1, 3).toDouble(), 'Cube');
-                        },
-                        text: 'Cube',
-                      ),
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double no2) => no1 % no2, 'Remainder');
-                        },
-                        text: 'Remainder',
-                      ),
-                      CalculatorButton(
-                        onPressed: () {
-                          calculateResult((double no1, double no2) => pow(no1, no2).toDouble(), 'Power');
-                        },
-                        text: 'Power',
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      result,
-                      style: const TextStyle(
-                        fontSize: 25,
-                        color: Colors.green,
-                        fontWeight: FontWeight.bold,
+            child: Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      controller: no1Controller,
+                      style: const TextStyle(fontSize: 20, color: Colors.black),
+                      decoration: InputDecoration(
+                        labelText: 'Enter number 1',
+                        border: OutlineInputBorder(),
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 10),
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      controller: no2Controller,
+                      style: const TextStyle(fontSize: 20, color: Colors.black),
+                      decoration: InputDecoration(
+                        labelText: 'Enter number 2',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // ... (existing code)
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // ... (existing code)
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        result,
+                        style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -160,7 +113,7 @@ class _DashBoardScreenState extends State<DashBoardScreen> {
     try {
       var no1 = double.parse(no1Controller.text);
       var no2 = double.parse(no2Controller.text);
-      double resultValue = operation(no1, no2); // Cast the result to double explicitly
+      double resultValue = operation(no1, no2);
       setState(() {
         result = 'The $operationName of $no1 and $no2 is ${resultValue.toStringAsFixed(2)}';
       });
